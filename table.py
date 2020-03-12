f89e6ad629c3a80fdbfe6ed0a7d8d4969fa203a6
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-12 18:22:07 +0530

    table of integer

---
+++ table.py
@@ -0,0 +1,7 @@
+n = int(input("enter the integer: "))
+
+i = 1    
+while i<=10:
+
+    print(n,"*",i,"=",n*i)
+    i = i+1
---
+++ tablefor.py
@@ -0,0 +1,7 @@
+n = int(input("enter the number: "))
+x = None
+
+for i in x:
+    i==1 and i<=10
+    print(n,"*",i,"=",n*i)
+    i = i+1
