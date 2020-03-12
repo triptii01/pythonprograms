f9f0b8658508d05c41c8e8e441393a63fa264270
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-12 18:22:57 +0530

    for loop

---
+++ forcount.py
@@ -0,0 +1,4 @@
+n = int(input("enter the integer: "))
+
+for i in range(1,n):
+     print(i)
---
+++ fortab.py
@@ -0,0 +1,4 @@
+n = int(input("enter the integer: "))
+
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
