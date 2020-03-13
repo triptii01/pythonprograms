b70eec6c6b5aa278fc10f01aa929220279069971
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-13 14:16:50 +0530

    Add files via upload

---
+++ sq.py
@@ -0,0 +1,24 @@
+#this is a stack
+stack = [4,6,3,7,8]
+print(stack)
+
+#adding last element in the stack
+stack.append(9)  
+print(stack)
+
+#removing first element from the stack
+stack.remove(4)
+print(stack)
+
+
+#this is a queue
+queue = [44,54,76,23,98]
+print(queue)
+
+#adding first element in the queue
+queue.insert(0,24)
+print(queue)
+
+#removing first element from the queue
+queue.pop(0)
+print(queue)
