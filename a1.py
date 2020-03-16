b04f6176c34d1255b6ee499786dbdc7ed23517da
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-16 14:10:09 +0530

    comparison b/w numbers that are user defined

--- a1.py
+++ a1.py
@@ -1,8 +1,9 @@
-
+#user defined inputs
 a = int(input("enter the value of a: "))
 
 b = int(input("enter the value of b: "))
 
+#comparison between two numbers
 if a>b:
 	print("a is maximum")
 else:
