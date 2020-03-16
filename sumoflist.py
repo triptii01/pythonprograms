29c2d9d9aef8af603f113ffb325e50481937c10c
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-16 11:32:43 +0530

    sum of elements in the list

---
+++ sumoflist.py
@@ -0,0 +1,14 @@
+#program to sum all the items in the list
+x = [54,32,6,18]
+print(x)
+
+total = sum(x)
+print(total)
+
+#program to get the largest number from the list
+largest_num = max(x)
+print(largest_num)
+
+#program to get the smallest number from the list
+smallest_num = min(x)
+print(smallest_num)
