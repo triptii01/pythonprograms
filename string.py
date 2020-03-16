f9947ae4805c0f1837c45619a2fd8cdfd2af69f1
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-16 23:29:08 +0530

    o/p=elements of same first and last value in list

---
+++ string.py
@@ -0,0 +1,10 @@
+#this is a list of strings
+l = ['sdfgbfvs','fghnfg','897438','jhfgdfj','eggrb','3454653']
+print(l)
+
+#to print elements of list having same first and last values of string
+for i in l :
+   if i[0] == i[-1]:
+      print(i)
+   
+
