90f416c3ae3c70550ea30755c552f03752bec0a5
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-17 23:17:48 +0530

    len(l1)>len(l2), print(l1)

---
+++ str.py
@@ -0,0 +1,10 @@
+#if length of list1 is greater than list2, print list1
+x = ['sdaefsd','dweffs','aergf','dWAEFGR','ewqrw','wereg']
+print(len(x))
+
+l = ['xyz','abc','ghi','mno']
+print(len(l))
+
+if len(x) > len(l):
+   print(x)
+
