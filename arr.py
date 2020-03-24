58e13675fd489834d268bdc0b5fa8037c571edc1
triptii01 <55341068+triptii01@users.noreply.github.com>
2020-03-24 11:44:24 +0530

    remove an element from arr using index value

--- arr.py
+++ arr.py
@@ -1,31 +1,5 @@
-#arrays in python
-#basics
-
-#import all the functions of array
-#'*' is for all the functions
+#remove element from the array using index value 
 from array import *
-
-#'i' signifies that it's an integer array
-vals = array('i',[5,4,7,2])
-#print the array
-print(vals)
-
-
-#check the type of array
-print(vals.typecode)
-
-#to know the address and size of the array
-print(vals.buffer_info())
-
-#reverse the array
-vals.reverse()
-print(vals)
-
-#check the index value of the element from the array
-#as the array is reversed so the values will change now
-print(vals[0])
-
-#to copy the array
-newArr = array(vals.typecode, ( a for a in vals ))
-for e in newArr:
-   print(e)
+arr = array('i',[2,4,5,6,7])
+arr.pop(0)
+print(arr)
