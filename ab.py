7190ecd58a8330f9035872ea06e06555143bff00
Tripti Choubey <55341068+triptii01@users.noreply.github.com>
2020-05-30 00:10:42 +0530

    extracting link from a string

--- ab.py
+++ ab.py
@@ -3,14 +3,16 @@ t='<a href="http://www.google.com"'
 u='<a href="http://www.googleweree.com"'
 v='<a href="http://www.googlasdasd.com"'
 
-def link(s) :
-	x = (s.find('"'))
+def link(l) :
+	x = (l.find('"'))
 	print(x)
-	y = s.find('"', x+1)
+	y = l.find('"', x+1)
 	print(y)
-	print(s[x+1:y])
+	print(l[x+1:y])
 
 link(s)
 link(t)
 link(u)
 link(v)
+
+
